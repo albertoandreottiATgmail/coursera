e7bcf4b9a061db146575fb01792121dc100fc6b4
Jose Pablo Alberto Andreotti <albertoandreotti@gmail.com>
2014-04-05 13:47:48 -0300

    PA5 from Algorithms I now working with a good mark in Coursera!

--- algorithms1/pe5/src/KdTree.java
+++ algorithms1/pe5/src/KdTree.java
@@ -1,138 +1,288 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
-import java.util.TreeMap;
+
 public class KdTree {
-	
-	
-	class KdNode {
-		
+
+	private class KdNode {
+
 		private KdNode right, left;
 		private Point2D local;
-		
-		public KdNode(KdNode right, KdNode left, Point2D p){
-			local = p;
+		private int size = 1;
+		private Comparator<Point2D> cmp;
+		protected RectHV rect;
+
+		public double distance(Point2D p) {
+			return rect.distanceTo(p);
 		}
-		
-		public KdNode(Point2D p){
+
+		public KdNode(Point2D p, Comparator<Point2D> comparator, RectHV l,
+				RectHV r) {
 			local = p;
-			left = new EmptyNode();
-			right = new EmptyNode();
+			left = new EmptyNode(l);
+			right = new EmptyNode(r);
+			rect = new RectHV(l.xmin(), l.ymin(), r.xmax(), r.ymax());
+			cmp = comparator;
 		}
-		
+
 		public KdNode() {
-			
+
 		}
-		
-		public int size(){
-			
-			return left.size() + right.size();
+
+		public int size() {
+			return size;
 		}
-		
-		public KdNode putX(Point2D k){
-			
-		if (local.compareTo(k) < 0)
-			return right.putY(k);
-		else if (local.x() < k.x())
-			left
-		else return this;
-			
+
+		public KdNode putX(Point2D k) {
+
+			if (local.compareTo(k) == 0)
+				return this;
+			int cmp = Point2D.X_ORDER.compare(k, local);
+			if (cmp > 0)
+				right = right.putY(k);
+			else
+				left = left.putY(k);
+
+			size = right.size() + left.size() + 1;
 			return this;
-			
+		}
+
+		public KdNode putY(Point2D k) {
+			if (local.compareTo(k) == 0)
+				return this;
+
+			int cmp = Point2D.Y_ORDER.compare(k, local);
+			if (cmp > 0)
+				right = right.putX(k);
+			else
+				left = left.putX(k);
+
+			size = right.size() + left.size() + 1;
+			return this;
+
+		}
+
+		public Point2D nearest(Point2D p) {
+			return nearest(p, local);
+		}
+
+		public Point2D nearest(Point2D p, Point2D best) {
+
+			Point2D localbest = closer(local, best, p);
+
+            if(cmp.compare(local, p) < 0){
+            	localbest = explore(right, p, localbest);
+            	localbest = explore(left, p, localbest);
+            }
+            else{
+            	localbest = explore(left, p, localbest);
+            	localbest = explore(right, p, localbest);
+            }
+
+			return localbest;
+
 		}
 		
-		public KdNode putY(Point2D k){
-			
+		public Point2D explore(KdNode k, Point2D p, Point2D best){
+			if (k.rect.distanceTo(p) < p.distanceTo(best))
+				return closer(k.nearest(p, best), best, p);
+			return best;
 		}
-		
+
+		private Point2D closer(Point2D a, Point2D b, Point2D c) {
+
+			return a.distanceTo(c) < b.distanceTo(c) ? a : b;
+
+		}
+
+		public KdNode getX(Point2D k) {
+
+			if (local.compareTo(k) == 0)
+				return this;
+
+			int cmp = Point2D.X_ORDER.compare(k, local);
+
+			if (cmp > 0)
+				return right.getY(k);
+			else
+				return left.getY(k);
+
+		}
+
+		public KdNode getY(Point2D k) {
+
+			if (local.compareTo(k) == 0)
+				return this;
+
+			int cmp = Point2D.Y_ORDER.compare(k, local);
+
+			if (cmp > 0)
+				return right.getX(k);
+			else
+				return left.getX(k);
+
+		}
+
+		public void draw() {
+			left.draw();
+			StdDraw.point(local.x(), local.y());
+			right.draw();
+		}
+
+		public List<Point2D> range(RectHV r, List<Point2D> acc) {
+
+			if (r.contains(local))
+				acc.add(local);
+
+			if (left.rect.intersects(r))
+				left.range(r, acc);
+
+			if (right.rect.intersects(r))
+				right.range(r, acc);
+
+			return acc;
+		}
+
+
 	}
-	
-	class EmptyNode extends KdNode{
-		
-		public EmptyNode(){
-			
+
+	private class EmptyNode extends KdNode {
+
+		public EmptyNode(RectHV r) {
+			rect = r;
 		}
-		
-		public int size(){
+
+		public Point2D nearest(Point2D p, Point2D best) {
+
+			return best;
+		}
+
+		public List<Point2D> range(RectHV rect, List<Point2D> acc) {
+
+			return acc;
+		}
+
+		public void draw() {
+
+		}
+
+		public KdNode putX(Point2D p) {
+			return new KdNode(p, Point2D.X_ORDER, new RectHV(rect.xmin(),
+					rect.ymin(), p.x(), rect.ymax()), new RectHV(p.x(),
+					rect.ymin(), rect.xmax(), rect.ymax()));
+		}
+
+		public KdNode putY(Point2D p) {
+			return new KdNode(p, Point2D.Y_ORDER, new RectHV(rect.xmin(),
+					rect.ymin(), rect.xmax(), p.y()), new RectHV(rect.xmin(),
+					p.y(), rect.xmax(), rect.ymax()));
+		}
+
+		public KdNode getX(Point2D p) {
+			return null;
+		}
+
+		public KdNode getY(Point2D p) {
+			return null;
+		}
+
+		public int size() {
 			return 0;
 		}
 	}
-	
+
 	private KdNode root;
-	private Point2D origin;
-	
-	// construct an empty set of points
-	public KdTree() {
-        origin = new Point2D(0.0, 0.0);
-        root = new KdNode();
-	}
 
 	// is the set empty?
 	public boolean isEmpty() {
-		return root.size() == 0;
+		return root == null;
 	}
-	
+
 	// number of points in the set
 	public int size() {
-		return root.size();
+
+		return root == null ? 0 : root.size();
 	}
 
 	// add the point p to the set (if it is not already in the set)
 	public void insert(Point2D p) {
-        root.put(p);
+
+		if (p.x() < 0 || p.y() < 0)
+			return;
+
+		if (root == null)
+			root = new KdNode(p, Point2D.X_ORDER, new RectHV(0.0, 0.0, p.x(),
+					1.0), new RectHV(p.x(), 0.0, 1.0, 1.0));
+		else
+			root.putX(p);
 	}
 
 	// does the set contain the point p?
 	public boolean contains(Point2D p) {
-		
+		return root.getX(p) != null;
 	}
 
 	// draw all of the points to standard draw
 	public void draw() {
 		StdDraw.show(0);
-        for(Point2D point: treeMap.values()) {
-		    StdDraw.point(point.x(), point.y());
-        }
+		if (root != null)
+			root.draw();
 	}
 
+	private class KdTreeIteratorFactory implements Iterable<Point2D> {
 
-	class KdTreeIteratorFactory implements Iterable<Point2D>{
-		
 		private List<Point2D> values;
-		public KdTreeIteratorFactory(List<Point2D> list){
+
+		public KdTreeIteratorFactory(List<Point2D> list) {
 			values = list;
 		}
+
 		@Override
-        public Iterator<Point2D> iterator(){
-        	return values.iterator();
-        }
+		public Iterator<Point2D> iterator() {
+			return values.iterator();
+		}
 	}
-	
+
 	// all points in the set that are inside the rectangle
 	public Iterable<Point2D> range(RectHV rect) {
 		List<Point2D> matches = new ArrayList<Point2D>();
-		
-		for (Point2D point: treeMap.values()){
-			if(rect.contains(point))
-				matches.add(point);
-		}
-		
+		root.range(rect, matches);
 		return new KdTreeIteratorFactory(matches);
 	}
 
 	// a nearest neighbor in the set to p; null if set is empty
 	public Point2D nearest(Point2D p) {
-		Point2D candidate = null;
-		double min = Double.MAX_VALUE;
+		return root.nearest(p);
+	}
+
+	public static void main(String[] args) {
+
+		Point2D point;
+		KdTree tree = new KdTree();
 		
-		for (Point2D point: treeMap.values())
-			if(point.distanceTo(p)<min) {
-			min = point.distanceTo(p);
-			candidate = point;
-			}
+		for (int i = 0; i < 1200000; i++) {
+			point = new Point2D(Math.random(), Math.random());
+			tree.insert(point);
+			assert (tree.contains(point)): "error on" + i;
+    	}
 		
-		//null returned when the set is empty.
-		return candidate;
+		tree.insert(new Point2D(0.499, 0.499));
+		
+		final long startTime = System.currentTimeMillis();
+		
+		for (int i = 0; i < 10000; i++) {
+						
+			point = new Point2D(Math.random(), Math.random());
+			
+			assert (tree.nearest(point).distanceTo(point)<0.02): "error on" + i;
+    	}
+		
+			
+		
+		final long endTime = System.currentTimeMillis();
+		System.out.println("Average execution time: " + (endTime - startTime)/10000.0 );
+		
+
 	}
 }
- 
--- algorithms1/pe5/src/PointSET.java
+++ algorithms1/pe5/src/PointSET.java
@@ -1,68 +1,98 @@
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class PointSET {
-	
-	private TreeMap<Double, Point2D> treeMap;
-	private Point2D origin;
-	
+
+	private class PointKey implements Comparable<PointKey> {
+
+		private Point2D origin = new Point2D(0, 0);
+		private Point2D local;
+		private double norm;
+
+		public void draw() {
+
+			StdDraw.point(local.x(), local.y());
+		}
+
+		public PointKey(Point2D p) {
+			local = p;
+			norm = p.distanceTo(origin);
+		}
+
+
+		@Override
+		public int compareTo(PointKey other) {
+
+			if (local.compareTo(other.local) == 0)
+				return 0;
+			if (this.norm < other.norm)
+				return -1;
+			if (this.norm > other.norm)
+				return 1;
+			return Point2D.X_ORDER.compare(local, other.local);
+		}
+
+	}
+
+	private TreeSet<PointKey> treeSet;
+
 	// construct an empty set of points
 	public PointSET() {
-        treeMap = new TreeMap<Double, Point2D>();
-        origin = new Point2D(0.0, 0.0);
+		treeSet = new TreeSet<PointKey>();
 	}
 
 	// is the set empty?
 	public boolean isEmpty() {
-		return treeMap.isEmpty();
+		return treeSet.isEmpty();
 	}
-	
+
 	// number of points in the set
 	public int size() {
-		return treeMap.size();
+		return treeSet.size();
 	}
 
 	// add the point p to the set (if it is not already in the set)
 	public void insert(Point2D p) {
-        treeMap.put(p.distanceTo(origin), p);
+		treeSet.add(new PointKey(p));
 	}
 
 	// does the set contain the point p?
 	public boolean contains(Point2D p) {
-		return treeMap.containsKey(p.distanceTo(origin));
+		return treeSet.contains(new PointKey(p));
 	}
 
 	// draw all of the points to standard draw
 	public void draw() {
 		StdDraw.show(0);
-        for(Point2D point: treeMap.values()) {
-		    StdDraw.point(point.x(), point.y());
-        }
+		for (PointKey point : treeSet)
+			point.draw();
 	}
 
+	private class PointSETFactory implements Iterable<Point2D> {
 
-	class PointSETFactory implements Iterable<Point2D>{
-		
 		private List<Point2D> values;
-		public PointSETFactory(List<Point2D> list){
+
+		public PointSETFactory(List<Point2D> list) {
 			values = list;
 		}
+
 		@Override
-        public Iterator<Point2D> iterator(){
-        	return values.iterator();
-        }
+		public Iterator<Point2D> iterator() {
+			return values.iterator();
+		}
 	}
-	
+
 	// all points in the set that are inside the rectangle
 	public Iterable<Point2D> range(RectHV rect) {
 		List<Point2D> matches = new ArrayList<Point2D>();
-		
-		for (Point2D point: treeMap.values()){
-			if(rect.contains(point))
-				matches.add(point);
+
+		for (PointKey pkey : treeSet) {
+			if (rect.contains(pkey.local))
+				matches.add(pkey.local);
 		}
-		
+
 		return new PointSETFactory(matches);
 	}
 
@@ -70,20 +100,14 @@ public class PointSET {
 	public Point2D nearest(Point2D p) {
 		Point2D candidate = null;
 		double min = Double.MAX_VALUE;
-		
-		for (Point2D point: treeMap.values())
-			if(point.distanceTo(p)<min) {
-			min = point.distanceTo(p);
-			candidate = point;
+
+		for (PointKey pkey: treeSet)
+			if (pkey.local.distanceTo(p) < min) {
+				min = pkey.local.distanceTo(p);
+				candidate = pkey.local;
 			}
-		
-		//null returned when the set is empty.
+
+		// null returned when the set is empty.
 		return candidate;
 	}
 }
- 
-
-
-
-
-
